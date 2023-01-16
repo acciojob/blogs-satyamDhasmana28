e4b27aeda84e7352cbe4f98fd3951891fb162201
Your Name <satyam.dhasmanaa@gmail.com>
2023-01-16 22:16:56 +0530

    try7

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -3,6 +3,7 @@ package com.driver.models;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -22,7 +23,7 @@ public class Blog {
 
 //    connecting to image
     @OneToMany(mappedBy = "blog",cascade = CascadeType.ALL)
-    private List<Image> imageList;
+    private List<Image> imageList = new ArrayList<>();
 //    getter,setter
 
     public int getId() {
